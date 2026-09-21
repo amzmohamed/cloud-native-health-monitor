c989fe3e03cac825866deaf5166a5d9cc00393b0
amzmohamed <161719861+amzmohamed@users.noreply.github.com>
2026-09-21 20:21:21 +0300

    fix(iam): use wildcard match for repository oidc sub claim

--- terraform/oidc.tf
+++ terraform/oidc.tf
@@ -24,7 +24,7 @@ resource "aws_iam_role" "github_actions_role" {
             "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com"
           }
           StringLike = {
-            "token.actions.githubusercontent.com:sub" : "repo:*:*cloud-native-health-monitor:*"
+            "token.actions.githubusercontent.com:sub" : "*cloud-native-health-monitor*"
           }
         }
       }
